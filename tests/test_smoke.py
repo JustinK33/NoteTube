de9187f6e5a4c3c2520eb38de861bf2f55957be7
Justin <justin022306@gmail.com>
2026-02-16 14:23:15 -0500

    bug: fixed missing module error for test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,13 @@
+import os
+import sys
+import django
 from django.test import TestCase
 from django.urls import reverse
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "Backend"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notetube.settings")
+django.setup()
 
 
 class SmokeTest(TestCase):
